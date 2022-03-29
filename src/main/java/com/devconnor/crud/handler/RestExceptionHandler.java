916963ffaeb14e41c053874cdd9eb62eddd7de8c
Connor <kor.connor@gmail.com>
2022-03-29 11:39:22 +0900

    n

--- src/main/java/com/devconnor/crud/handler/RestExceptionHandler.java
+++ src/main/java/com/devconnor/crud/handler/RestExceptionHandler.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.xml.ws.Response;
-
 @RestController
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
